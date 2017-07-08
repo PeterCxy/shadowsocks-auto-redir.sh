33795e523c3b5cdeab0798bda4fc1071edcf3b6d
KireinaHoro <i@jsteward.moe>
2017-07-08 16:45:25 +0800

    update install scripts for openrc [/3]

--- install-openrc.sh
+++ install-openrc.sh
@@ -6,6 +6,11 @@ link() {
     ln -s /etc/init.d/shadowsocks-auto-redir /etc/init.d/shadowsocks-auto-redir.v6
 }
 
+config() {
+    cp /etc/shadowsocks-libev/redir.json /etc/shadowsocks-libev/redir.v4.json
+    sed -e "s/127.0.0.1/::1/" /etc/shadowsocks-libev/redir.json > /etc/shadowsocks-libev/redir.v6.json
+}
+
 unlink() {
     rm -rf /opt/shadowsocks-auto-redir
     rm -rf /etc/init.d/shadowsocks-auto-redir*
@@ -28,4 +33,6 @@ elif [[ "$1" == "unlink" ]]; then
     unlink
 elif [[ "$1" == "update" ]]; then
     update
+elif [[ "$1" == "config" ]]; then
+    config
 fi
